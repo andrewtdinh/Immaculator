8df245b013bca67c32662516ec2d01f85535e13a
Andrew Dinh <andrewdinh74@yahoo.com>
2015-06-11 19:02:18 -0700

    Fix font size adjustment for different levels

--- client/index.js
+++ client/index.js
@@ -19,7 +19,8 @@ function clickNumber(){
   var num = $(this).text();
   var display = $('#display').text();
   console.log('current display: ', display);
-  var output = (display === '0') ? num : display + num;
+  // var output = (display === '0') ? num : display + num;
+  var output = (display === '0') ? num : (display.length < 18 ? (display + num) : display);
   adjustFont(output);
   $('#display').text(output);
 }
@@ -76,25 +77,28 @@ function adjustFont(stringOrNum){
   console.log('currentFontSize: ', currentFontSize);
   if (typeof stringOrNum === 'number') {display = stringOrNum.toString();}
   else {display = stringOrNum;}
-  if (display.length < 9) {
-    if (currentFontSize !== '60px') {
-      $('#display').css('font-size', '60px');
-      $('#display').css('height', '40px');
-      $('#display').css('top', '30px');
+  if (display.length < 10) {
+    if (currentFontSize !== '6rem') {
+      $('#display').css('font-size', '6rem');
+      $('#display').css('padding-top', '2.8vh');
+      // $('#display').css('height', '40px');
+      // $('#display').css('top', '30px');
     }
   }
-  else if (display.length < 14) {
-    if (currentFontSize !== '30px'){
-      $('#display').css('font-size', '30px');
-      $('#display').css('height', '30px');
-      $('#display').css('top', '45px');
+  else if (display.length < 15) {
+    if (currentFontSize !== '4rem'){
+      $('#display').css('font-size', '4rem');
+      $('#display').css('padding-top', '5.6vh');
+      // $('#display').css('height', '30px');
+      // $('#display').css('top', '45px');
     }
   }
   else {
-    if (currentFontSize !== '20px'){
-      $('#display').css('font-size', '20px');
-      $('#display').css('height', '20px');
-      $('#display').css('top', '55px');
+    if (currentFontSize !== '3rem'){
+      $('#display').css('font-size', '3rem');
+      $('#display').css('padding-top', '6.6vh');
+      // $('#display').css('height', '20px');
+      // $('#display').css('top', '55px');
     }
   }
 }
