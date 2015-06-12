94fb7dd544f62f7f4f9a581cbbfa24d45cd108ac
Andrew Dinh <andrewdinh74@yahoo.com>
2015-06-11 19:08:16 -0700

    Remove redundant lines before fixing calculator operation problems

--- client/index.js
+++ client/index.js
@@ -18,8 +18,6 @@ function init(){
 function clickNumber(){
   var num = $(this).text();
   var display = $('#display').text();
-  console.log('current display: ', display);
-  // var output = (display === '0') ? num : display + num;
   var output = (display === '0') ? num : (display.length < 18 ? (display + num) : display);
   adjustFont(output);
   $('#display').text(output);
@@ -38,7 +36,6 @@ function clear(){
 }
 
 function clickOperator(){
-  // debugger;
   memory = parseFloat($('#display').text());
   operation = $(this).text();
   $('#display').text('0');
@@ -81,24 +78,18 @@ function adjustFont(stringOrNum){
     if (currentFontSize !== '6rem') {
       $('#display').css('font-size', '6rem');
       $('#display').css('padding-top', '2.8vh');
-      // $('#display').css('height', '40px');
-      // $('#display').css('top', '30px');
     }
   }
   else if (display.length < 15) {
     if (currentFontSize !== '4rem'){
       $('#display').css('font-size', '4rem');
       $('#display').css('padding-top', '5.6vh');
-      // $('#display').css('height', '30px');
-      // $('#display').css('top', '45px');
     }
   }
   else {
     if (currentFontSize !== '3rem'){
       $('#display').css('font-size', '3rem');
       $('#display').css('padding-top', '6.6vh');
-      // $('#display').css('height', '20px');
-      // $('#display').css('top', '55px');
     }
   }
 }
